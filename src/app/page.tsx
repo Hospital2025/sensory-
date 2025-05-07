6f425140682ef0b8762716efa620e4aefcc636ec
Rey-24-rey <rainbowsogiesc.community@gmail.com>
2025-05-07 14:45:42 +0300

    images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,9 @@ export default function Home() {
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
           {[
-            { title: 'Haircut & Grooming', img: '/haircut.jpg' },
+            { title: 'Haircut & Grooming', img: '/cut.jpg' },
             { title: 'Spa Treatments', img: '/spa.jpg' },
-            { title: 'Pedicure & Manicure', img: '/nails.jpg' },
+            { title: 'Pedicure & Manicure', img: '/nail.jpg' },
           ].map((service) => (
             <div
               key={service.title}
